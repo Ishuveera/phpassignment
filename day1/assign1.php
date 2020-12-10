25cb95029c7fa7323269b9e661eb78d45b51754c
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2020-12-10 12:52:41 +0530

    Create assign1.php

---
+++ day1/assign1.php
@@ -0,0 +1,12 @@
+<?php
+   $num = 1;  
+   for ($i = 0; $i < 4; $i++)
+   {
+      for ($j = 0; $j <= $i; $j++ )
+      {
+         echo "$num ";
+         $num = $num + 1;
+      }
+      echo  "<br/>";
+   }
+?>
